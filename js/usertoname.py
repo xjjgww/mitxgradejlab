b9fb6ef902702c73d7ea77616de3428e3341bada
xjjgww <xjjgww@gmail.com>
2020-02-12 18:10:46 -0500

    add 8.13 name list

--- js/usertoname.py
+++ js/usertoname.py
@@ -5,20 +5,26 @@ import json
 
 session = HTMLSession()
 
-html = open("name-table.html", "r").read()
-parsed_html = BeautifulSoup(html, "html.parser")
-trs = parsed_html.find_all('a')
-
+htmls = ["name-table-814.html", "name-table-813.html"]
 names = {}
-for user in trs:
-    kerberos = user.text
-    print(kerberos+' ...')
-    url = "https://web.mit.edu/bin/cgicso?options=general&query=" + kerberos
-    r = session.get(url)
-    sel = 'body > #contents > #main > table > tr > td > table > tr > td > pre'
-    prename = list(r.html.find(sel))[0]
-    name = (re.search(r'name:\s(.+?)\semail.+', prename.text))[1]
-    names[kerberos] = name
+
+for filename in htmls:
+    html = open(filename, "r").read()
+    parsed_html = BeautifulSoup(html, "html.parser")
+    trs = parsed_html.find_all('a')
+
+    for user in trs:
+        kerberos = user.text
+        url = "https://web.mit.edu/bin/cgicso?options=general&query=" + kerberos
+        r = session.get(url)
+        sel = 'body > #contents > #main > table > tr > td > table > tr > td > pre'
+        prename = list(r.html.find(sel))[0]
+        print(kerberos+" -- "+prename.text)
+        parsename = re.search(r'name:\s(.+?)\semail.+', prename.text)
+        if parsename == None: continue
+        name = parsename[1]
+        names[kerberos] = name
+        print(kerberos+' ... '+name)
 
 print(names)
 
